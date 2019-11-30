f2b6ccdb223cd97d71cb1b95715c7f90a49c2421
Mahrukh25 <mahrukhimtiaz92@yahoo.com>
2019-12-01 00:58:36 +0500

    First code

---
+++ f1.py
@@ -0,0 +1,18 @@
+print ("Pakistan Zindabad")
+print ("We are Pakistan")
+
+
+
+
+name = input "Enter your name:"
+fname = input "Enter Father name:"
+uni = input "Enter university name:"
+
+info = """
+Name : {}
+Father name : {}
+University : {}
+
+"""
+card = info.format(name, fname, uni)
+print (card)
